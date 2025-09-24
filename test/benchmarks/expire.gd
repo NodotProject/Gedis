af3c626e81b0954ee313d8e4a0e6ecc82851f5fa
Jake Cattrall <623463+krazyjakee@users.noreply.github.com>
2025-09-25 00:52:00 +0100

    even more efficiency

--- test/benchmarks/expire.gd
+++ test/benchmarks/expire.gd
@@ -10,33 +10,27 @@ func _init():
 		printerr("Failed to instantiate Gedis.")
 		quit(1)
 
-	benchmark_key_expiry(gedis)
+	benchmark_key_expiry(gedis, 1)
+	benchmark_key_expiry(gedis, 2)
+	benchmark_key_expiry(gedis, 3)
+	benchmark_key_expiry(gedis, ITERATIONS)
 
 	gedis.queue_free()
 	quit()
 
-func benchmark_key_expiry(gedis: Gedis):
+func benchmark_key_expiry(gedis: Gedis, multiplier: int):
 	var time_source = GedisProcessDeltaTimeSource.new()
 	gedis.set_time_source(time_source)
 
 	for i in range(ITERATIONS):
 		var key := "key" + str(i)
-		gedis.set_value(key, "value")
-		gedis.expire(key, 10 + i)
+		gedis.setex(key, i, "value")
 
-	var start_time := Time.get_ticks_msec()
+	time_source.current_time += (ITERATIONS / multiplier) * 1000
+
+	var start_time = Time.get_ticks_msec()
 	gedis._expiry._purge_expired()
-	var end_time := Time.get_ticks_msec()
+	var end_time = Time.get_ticks_msec()
 
 	var elapsed_time = end_time - start_time
-	print("benchmark_key_pre_expiry: {0}ms for {1} iterations".format([elapsed_time, ITERATIONS]))
-
-	# Advance time past the expiry point of all keys
-	time_source.current_time += (10 + ITERATIONS) * 1000
-
-	start_time = Time.get_ticks_msec()
-	gedis._expiry._purge_expired()
-	end_time = Time.get_ticks_msec()
-
-	elapsed_time = end_time - start_time
-	print("benchmark_key_expiry: {0}ms for {1} iterations".format([elapsed_time, ITERATIONS]))
+	print("benchmark_key_expiry: {0}ms for {1} iterations of {2}".format([elapsed_time, ITERATIONS / multiplier, ITERATIONS]))
